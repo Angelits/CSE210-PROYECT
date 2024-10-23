4058f836b7bc40d0ad2559f6b3a9692088208c1a
yourusername <youremail@>
2024-10-23 13:16:24 -0600

    done

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        
+        shapes.Add(new Square("Red", 8));
+        shapes.Add(new Rectangle("Blue", 2, 1));
+        shapes.Add(new Circle("Green", 6));
+
+        
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"The shape with {shape.GetColor()} color has an area of: {shape.GetArea()}");
+        }
     }
 }
