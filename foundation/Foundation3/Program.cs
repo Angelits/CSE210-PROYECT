405516e701d059af1372297a332cc2163fb0239c
yourusername <youremail@>
2024-10-28 19:19:18 -0600

    updated and finished code

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -1,15 +1,16 @@
 using System;
 using System.Collections.Generic;
 
+
 public abstract class Activity
 {
-    protected DateTime date;
-    protected int duration;
+    protected DateTime _date;
+    protected int _duration; 
 
     public Activity(DateTime date, int duration)
     {
-        this.date = date;
-        this.duration = duration;
+        _date = date;
+        _duration = duration;
     }
 
     public virtual double GetDistance()
@@ -19,86 +20,86 @@ public abstract class Activity
 
     public virtual double GetSpeed()
     {
-        return 0;
+        return 0; 
     }
 
     public virtual double GetPace()
     {
         double distance = GetDistance();
-        return distance > 0 ? (duration / distance) : 0; 
+        return distance > 0 ? (_duration / distance) : 0; 
     }
 
     public virtual string GetSummary()
     {
-        return $"{date:dd MMM yyyy} - Duration: {duration} min";
+        return $"{_date:dd MMM yyyy} - Duration: {_duration} min";
     }
 }
 
 
 public class Running : Activity
 {
-    private double distance; 
+    private double _distance; 
 
     public Running(DateTime date, int duration, double distance) : base(date, duration)
     {
-        this.distance = distance;
+        _distance = distance;
     }
 
     public override double GetDistance()
     {
-        return distance;
+        return _distance;
     }
 
     public override double GetSpeed()
     {
-        return (distance / duration) * 60; 
+        return (_distance / _duration) * 60; 
     }
 
     public override string GetSummary()
     {
-        return base.GetSummary() + $" Running - Distance: {distance} miles, Speed: {GetSpeed():F1} mph, Pace: {GetPace():F2} min per mile";
+        return base.GetSummary() + $" Running - Distance: {_distance:F1} miles, Speed: {GetSpeed():F1} mph, Pace: {GetPace():F2} min per mile";
     }
 }
 
 
 public class Cycling : Activity
 {
-    private double speed; 
+    private double _speed; 
 
     public Cycling(DateTime date, int duration, double speed) : base(date, duration)
     {
-        this.speed = speed;
+        _speed = speed;
     }
 
     public override double GetDistance()
     {
-        return (speed * duration) / 60; 
+        return (_speed * _duration) / 60; 
     }
 
     public override string GetSummary()
     {
-        return base.GetSummary() + $" Cycling - Distance: {GetDistance():F1} miles, Speed: {speed} mph, Pace: {60 / speed:F2} min per mile";
+        return base.GetSummary() + $" Cycling - Distance: {GetDistance():F1} miles, Speed: {_speed} mph, Pace: {60 / _speed:F2} min per mile";
     }
 }
 
 
 public class Swimming : Activity
 {
-    private int laps;
+    private int _laps;
 
     public Swimming(DateTime date, int duration, int laps) : base(date, duration)
     {
-        this.laps = laps;
+        _laps = laps;
     }
 
     public override double GetDistance()
     {
-        return (laps * 50) / 1000.0 * 0.62; 
+        return (_laps * 50) / 1000.0 * 0.62; 
     }
 
     public override double GetSpeed()
     {
-        return (GetDistance() / duration) * 60; 
+        return (GetDistance() / _duration) * 60; 
     }
 
     public override string GetSummary()
@@ -114,9 +115,9 @@ class Program
     {
         List<Activity> activities = new List<Activity>
         {
-            new Running(new DateTime(2023, 10, 3), 30, 3.0),
-            new Cycling(new DateTime(2023, 11, 4), 45, 12.0),
-            new Swimming(new DateTime(2023, 12, 5), 30, 20)
+            new Running(new DateTime(2024, 2, 3), 30, 3.0),
+            new Cycling(new DateTime(2024, 5, 4), 45, 12.0),
+            new Swimming(new DateTime(2024, 6, 5), 30, 20)
         };
 
         foreach (var activity in activities)
